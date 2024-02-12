c995c981b995928e7a4863bd8f63ab1a548c4930
OzamaGenova <genovaoz@yandex.ru>
2024-02-12 15:57:15 +0400

    Ex 1 and Ex 2

---
+++ test/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace testprog
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Укажить путь к файлу");
+            string inputFilePath = Console.ReadLine();
+            string outputFilePath = "output.txt";
+
+            string text = File.ReadAllText(inputFilePath);
+
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            string[] words = text.Split(new char[] { ' ', '\n', '\r', '\t', '.', ',', ';', ':', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                string cleanedWord = word.ToLower();
+                if (wordCount.ContainsKey(cleanedWord))
+                    wordCount[cleanedWord]++;
+                else
+                    wordCount[cleanedWord] = 1;
+            }
+
+            var sortedWordCount = wordCount.OrderByDescending(w => w.Value);
+
+            using (StreamWriter outputFile = new StreamWriter(outputFilePath))
+            {
+                foreach (var word in sortedWordCount)
+                {
+                    outputFile.WriteLine($"{word.Key} {word.Value}");
+                }
+            }
+        }
+    }
+}
